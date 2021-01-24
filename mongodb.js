c7c02eaf705e47e5028d14a60fbb0e611889bb79
roshanpaturkar <paturkarr@gmail.com>
2021-01-24 23:36:51 +0530

    mongodb connection established

--- mongodb.js
+++ mongodb.js
@@ -1,2 +1,16 @@
 //  CRUD Operation's in MongoDB using NodeJS
 // Create Read Update Delete
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager-api'
+
+MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect database!')
+    }
+
+    console.log('Database connection established successfully!')
+})
