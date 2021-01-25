35b6b7fff533cf62c8ef6bd176e1e06e290de9ef
roshanpaturkar <paturkarr@gmail.com>
2021-01-26 03:23:38 +0530

    user routers separated

--- src/index.js
+++ src/index.js
@@ -4,81 +4,13 @@ const {ObjectID} = require('mongodb')
 
 const User = require('./models/user')
 const Task = require('./models/task')
-const { request, response } = require('express')
+const userRouter = require('./routers/user')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users', async (request, response) => {
-    const user = new User(request.body)
-
-    try {
-        await user.save()
-        response.status(201).send(user)
-    } catch (error) {
-        response.status(400).send(error)
-    }
-})
-
-app.get('/users', async (request, response) => {
-    try {
-        const users = await User.find({})
-        response.send(users)
-    } catch (error) {
-        response.status(500).send()
-    }
-})
-
-app.get('/users/:id', async (request, response) => {
-    const _id = request.params.id
-
-    try {
-        const user = await User.findById(_id)
-        
-        if (!user) {
-            return response.status(404).send()
-        }
-        response.send(user)
-    } catch (error) {
-        response.status(500).send()
-    }
-})
-
-app.patch('/users/:id', async (request, response) => {
-    const updates = Object.keys(request.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return response.status(400).send({error: 'Invalid updates!'})
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(request.params.id, request.body, {new: true, runValidators: true})
-
-        if (!user) {
-            return response.status(404).send()
-        }
-        response.send(user)
-    } catch (error) {
-        response.status(400).send(error)
-    }
-})
-
-app.delete('/users/:id', async (request, response) => {
-    try {
-        const user = await User.findByIdAndDelete(request.params.id)
-
-        if (!user) {
-            return response.status(404).send()
-        }
-        response.send(user)
-    } catch (error) {
-        response.status(500).send()   
-    }
-})
+app.use(userRouter)
 
 app.post('/tasks', async (request, response) => {
     const task = new Task(request.body)
