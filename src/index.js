c05b839e13248e629902bed63462dd24b8321264
roshanpaturkar <paturkarr@gmail.com>
2021-01-25 23:58:12 +0530

    task route implemented to add new task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 require('./db/mongoose')
+
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,6 +14,15 @@ app.post('/users', (request, response) => {
 
     user.save().then(() => {
         response.send(user)
+    }).catch((error) => {
+        response.status(400).send(error)
+    })
+})
+
+app.post('/tasks', (request, response) => {
+    const task = new Task(request.body)
+    task.save().then(() => {
+        response.send(task)
     }).catch((error) => {
         response.status(400).send(error)
     })
