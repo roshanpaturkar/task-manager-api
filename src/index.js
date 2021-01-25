964e176f6c07b24585da2c7b902b03dc0ef1512e
roshanpaturkar <paturkarr@gmail.com>
2021-01-25 23:27:33 +0530

    request data received

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+
+app.post('/users', (request, response) => {
+    console.log(request.body)
+    response.send('testing!')
+})
+
 app.listen(port, () => {
-    console.log('Server is up on port ' + port);
+    console.log('Server is up on port ' + port)
 })
