d939ac0b08f1a0b2c2ed080675ca75ec50006090
roshanpaturkar <paturkarr@gmail.com>
2021-01-27 18:10:47 +0530

    added authentication on update user profile

--- src/routers/user.js
+++ src/routers/user.js
@@ -53,7 +53,7 @@ router.get('/users/me', auth, async (request, response) => {
     response.send(request.user)
 })
 
-router.patch('/users/:id', async (request, response) => {
+router.patch('/users/me', auth, async (request, response) => {
     const updates = Object.keys(request.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -67,15 +67,10 @@ router.patch('/users/:id', async (request, response) => {
         //like userSchema.pre() and can not do any pre-operations before updates. thats why we use another way  
         //const user = await User.findByIdAndUpdate(request.params.id, request.body, {new: true, runValidators: true})
 
-        const user = await User.findById(request.params.id)
+        updates.forEach((update) => request.user[update] = request.body[update])
+        await request.user.save()
 
-        updates.forEach((update) => user[update] = request.body[update])
-        await user.save()
-
-        if (!user) {
-            return response.status(404).send()
-        }
-        response.send(user)
+        response.send(request.user)
     } catch (error) {
         response.status(400).send(error)
     }
