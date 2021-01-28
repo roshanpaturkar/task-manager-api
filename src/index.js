ab26b112c70e652d2636ddb574421e57f1b332db
roshanpaturkar <paturkarr@gmail.com>
2021-01-29 03:14:04 +0530

    final push for 1st production

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ require('./db/mongoose')
 
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-const { request } = require('express')
 
 const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
-app.get('', (request, response) => {
-    response.send('Welcome to Task Manager App')
-})
 app.use(userRouter)
 app.use(taskRouter)
 
