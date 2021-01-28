7a33e8281b245178b365566d1c9eaa90d5369082
roshanpaturkar <paturkarr@gmail.com>
2021-01-29 02:44:49 +0530

    home route added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ require('./db/mongoose')
 
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const { request, response } = require('express')
 
 const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
+app.get('', (request, response) => {
+    response.send('Welcome to Task Manager App (API)')
+})
 app.use(userRouter)
 app.use(taskRouter)
 
