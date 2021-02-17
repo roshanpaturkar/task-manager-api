fcfb12292f313bc9aed28ef9866b087164937b82
roshanpaturkar <paturkarr@gmail.com>
2021-02-17 14:22:52 +0530

    index.js reconfigured for express app config and listen on port

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-const express = require('express')
-const cors = require('cors')
-require('./db/mongoose')
-
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-app.use(cors())
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
