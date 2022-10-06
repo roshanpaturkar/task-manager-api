7ffe34980622bfedf35b5e91600e8776bb3e6a5d
Roshan Paturkar <paturkarr@gmail.com>
2022-10-06 15:21:32 +0530

    cors enabled

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+var origin = require('./cors/origin')
 require('./db/mongoose')
 
 const userRouter = require('./routers/user')
@@ -8,7 +9,12 @@ const taskRouter = require('./routers/task')
 
 const app = express()
 
-app.use(cors())
+//  CORS origins configuration
+if (process.env.ENV != undefined) {
+    app.use(cors())
+} else {
+    app.use(cors(origin))
+}
 
 app.use(express.json())
 app.use(userRouter)
---
+++ src/cors/origin.js
@@ -0,0 +1,13 @@
+var whitelist = ['http://localhost.com']
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+module.exports = corsOptions
